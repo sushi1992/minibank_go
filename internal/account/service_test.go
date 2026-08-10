54075e2fbe50bd332215ad7c9c79a6f267aed10a
Suzan Aydin <suzan_aydin7@hotmail.co.uk>
2026-08-11 00:44:18 +0100

    Add fake/moq version of repo and test service without concrete implementation (MemoryRepo)

--- internal/account/service_test.go
+++ internal/account/service_test.go
@@ -1,11 +1,30 @@
 package account
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
+
+type FakeRepository struct {
+	savedAccount *Account
+	saveErr      error
+}
+
+func (repo *FakeRepository) Save(account *Account) error {
+	repo.savedAccount = account
+	return repo.saveErr
+}
+
+func (repo *FakeRepository) Get(id string) (*Account, error) {
+	return nil, nil
+}
 
 func TestCreateAccountForService(t *testing.T) {
 	id := "Apple"
 	owner := "John Doe"
-	service := NewService(NewMemoryRepository())
+
+	repo := &FakeRepository{}
+	service := NewService(repo)
 	account, err := service.CreateAccount(id, owner, CurrencyGBP)
 
 	if err != nil {
@@ -28,12 +47,28 @@ func TestCreateAccountForService(t *testing.T) {
 		t.Errorf("expected '%s' but instead contains '%s'", CurrencyGBP, account.Currency)
 	}
 
-	savedAccount, err := service.repo.Get(id)
-	if savedAccount != account {
+	if repo.savedAccount != account {
 		t.Fatalf("saved account should have been the same as the created account")
 	}
+}
 
-	if err != nil {
-		t.Fatalf("retrieving account caused an error: %v", err)
+func TestCreateAccountFailsWhenSaveFailsForService(t *testing.T) {
+	id := "Apple"
+	owner := "John Doe"
+
+	saveErr := errors.New("error saving account")
+	repo := &FakeRepository{
+		saveErr: saveErr,
+	}
+
+	service := NewService(repo)
+	account, err := service.CreateAccount(id, owner, CurrencyGBP)
+
+	if !errors.Is(err, saveErr) {
+		t.Fatalf("expected original save error to be preserved, but instead got %v", err)
+	}
+
+	if account != nil {
+		t.Fatalf("account should have been nil but instead is %v", account)
 	}
 }
