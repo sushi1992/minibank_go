48daeec7cd2a75862ea8f06d0d0cee7c1c12b063
Suzan Aydin <suzan_aydin7@hotmail.co.uk>
2026-08-11 23:11:48 +0100

    Add more test logic

--- internal/account/transaction_process_test.go
+++ internal/account/transaction_process_test.go
@@ -15,6 +15,7 @@ func TestTransactionProcessorProcessesDeposit(t *testing.T) {
 	}
 
 	processor := NewTransactionProcessor(repo)
+	result := make(chan TransactionResult)
 
 	go processor.Run()
 
@@ -22,5 +23,16 @@ func TestTransactionProcessorProcessesDeposit(t *testing.T) {
 		AccountID: "Acc1",
 		Type:      TransactionDeposit,
 		Amount:    500,
+		Result:    result,
 	})
+
+	transactionResult := <-result
+
+	if transactionResult.Err != nil {
+		t.Fatalf("transaction failed: %v", transactionResult.Err)
+	}
+
+	if account.BalancePence != 500 {
+		t.Fatalf("expected balance 500, got %d", account.BalancePence)
+	}
 }
